2b4ef5f7d496f323c40207a0ee2b4278e2d2d070
r2d2c2 <r2d2b22@gmail.com>
2025-04-25 10:02:27 +0900

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Text = "이것은 \r\n 멀티라인 \r\n 태스트박스\r\n 입니다.";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
