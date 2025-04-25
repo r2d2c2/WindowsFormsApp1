80c88b5ab7d7f4ef9c9ef93078ec644602476e5a
Alice-Maeng <sssyyy0522@naver.com>
2025-04-25 11:18:47 +0900

    Revert "실습3 , 맹승연입니다."
    This reverts commit ba5a53bf806a6eeaebffde898d0a61a6aec586cc.

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,7 +19,6 @@ namespace WindowsFormsApp1
             MessageBox.Show("이것은 MessageBox입니다.");
             textBox1.Text = "이것은 \r\n 멀티라인 \r\n 태스트박스\r\n 입니다.!";
             textBox1.Text+= "\r\n 추가된 텍스트입니다.";
-            MessageBox.Show("안녕하세요, 맹승연입니다.");
 
         }
 
