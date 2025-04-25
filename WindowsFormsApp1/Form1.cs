5d955d4f213522170c19ce335a4065bd1633ed33
r2d2c2 <r2d2b22@gmail.com>
2025-04-25 10:16:30 +0900

    update test

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,7 +16,8 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
 
-            textBox1.Text = "이것은 \r\n 멀티라인 \r\n 태스트박스\r\n 입니다.";
+            textBox1.Text = "이것은 \r\n 멀티라인 \r\n 태스트박스\r\n 입니다.!";
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
